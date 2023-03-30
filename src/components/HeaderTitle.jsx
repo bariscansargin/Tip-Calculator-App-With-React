fc6fe4fc3ebea3020c9cbc6ccb31eba9c1895bab
Barışcan Sargın <bariscansargin4@gmail.com>
2023-03-31 02:48:56 +0300

    Created HeaderTitle component.

---
+++ src/components/HeaderTitle.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderTitle = () => {
+  return (
+    <React.Fragment>
+      <div className="my-12">
+        <h1 className="font-mono font-bold tracking-wide text-green-900	">
+          SPLI
+        </h1>
+        <h1 className="font-mono font-bold tracking-wide text-green-900	">
+          TTER
+        </h1>
+      </div>
+      <div></div>
+    </React.Fragment>
+  );
+};
+
+export default HeaderTitle;
