20b9c5ecb4008f479b16b2690b92734586028c80
Barışcan Sargın <bariscansargin4@gmail.com>
2023-03-31 03:49:15 +0300

    Modified Button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const buttonDefaultStyling = "focus:outline-none text-white bg-green-700 hover:bg-green-500 focus:ring-4 focus:ring-green-300 focus:bg-green-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 my-4"
+const buttonDefaultStyling =
+  "focus:outline-none text-white bg-green-800 hover:bg-green-500 focus:ring-4 focus:ring-green-300 focus:bg-green-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 my-4";
 
-const Button = ({children}) => {
-    return <button className={buttonDefaultStyling}>{children}</button>
-}
+const Button = ({ children, handleClick, value }) => {
+  return (
+    <button
+      value={value}
+      onClick={handleClick}
+      className={buttonDefaultStyling}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
